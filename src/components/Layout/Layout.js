8bbb996181eee4cbe289565ab88f2003268c6b64
jonathans0ramirez <jonathan200949@gmail.com>
2020-02-16 20:06:20 -0500

    Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = ( props ) => (
     <Auxiliary>
-        <div>Toolbar, SideDrawer, BackDrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
+            <Toolbar />
+            <SideDrawer />
+            <main className={classes.Content}>
+                {props.children}
+            </main>
     </Auxiliary>
 );
 
